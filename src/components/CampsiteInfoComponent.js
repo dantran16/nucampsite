57918211710d7843c80017034c6fb9a6e9a5a9ad
Dan Tran <dantranux@gmail.com>
2022-01-04 20:28:03 -0800

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,6 +16,7 @@ import {
   Link
 } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
+import { Loading } from './LoadingComponent'
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len)
@@ -158,6 +159,26 @@ const RenderComments = ({ comments, addComment, campsiteId }) => {
 };
 
 const CampsiteInfo = (props) => {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    )
+  }
+  if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    )
+  }
 	if (props.campsite) {
 		return (
 			<div className="container">
