deb0c14eff90665af74f1899bb130d19fb9046a4
Dan Tran <dantranux@gmail.com>
2022-01-06 16:33:55 -0800

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -17,6 +17,7 @@ import {
 } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent'
+import { baseUrl } from '../shared/baseUrl'
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len)
@@ -122,7 +123,7 @@ const RenderCampsite = ({ campsite }) => {
 	return (
 		<div className="col-md-5 m-1">
 			<Card>
-				<CardImg width="100%" top src={campsite.image} alt={campsite.name} />
+				<CardImg width="100%" top src={baseUrl + campsite.image} alt={campsite.name} />
 				<CardBody>
 					<CardText>{campsite.description}</CardText>
 				</CardBody>
