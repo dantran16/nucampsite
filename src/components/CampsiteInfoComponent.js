c5131f0433972f711e57069ee70caa6cb498f3c4
Dan Tran <dantranux@gmail.com>
2022-01-07 16:55:08 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -47,8 +47,8 @@ class CommentForm extends Component{
 	}
 
   handleSubmit(values) {
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
     this.toggleModal()
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
   }
 
   render() {
@@ -132,7 +132,7 @@ const RenderCampsite = ({ campsite }) => {
 	);
 };
 
-const RenderComments = ({ comments, addComment, campsiteId }) => {
+const RenderComments = ({ comments, postComment, campsiteId }) => {
 	if (comments) {
 		return (
 			<div className="col-md-5 m-1">
@@ -152,7 +152,7 @@ const RenderComments = ({ comments, addComment, campsiteId }) => {
 						</div>
 					);
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
 			</div>
 		);
 	}
@@ -199,7 +199,7 @@ const CampsiteInfo = (props) => {
 					<RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id} />
 				</div>
 			</div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from "../redux/ActionCreators";
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from "../redux/ActionCreators";
 
 
 const mapStateToProps = state => {
@@ -23,7 +23,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = {
-  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+  postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
   fetchCampsites: () => (fetchCampsites()),
   resetFeedbackForm: () => (actions.reset('feedbackForm')),
   fetchComments: () => (fetchComments()),
@@ -69,7 +69,7 @@ class Main extends Component {
             (comment) => comment.campsiteId === +match.params.campsiteId
           )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
 				/>
 			);
     };
